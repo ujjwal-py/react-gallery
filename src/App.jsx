912c389d404eb9268af61e89f136d1a542872bbd
17-ujjwal <ujjwalkumar612422@gmail.com>
2026-03-17 23:24:01 +0530

    feat: state handling using useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,128 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { useReducer, useEffect, useState } from 'react'
 import Card from './components/Card'
 import Api from './services/Api'
 
 
 function App() {
   const client_id = import.meta.env.VITE_CLIENT_ID;
-  const [sVal, setSval] = useState({
-    query: "",
-    order: 'relevance'
-  })
-  const [page, setPage] = useState(1);
-  const [search, setSearch] = useState(false);
+  // const [sVal, setSval] = useState({
+  //   query: "",
+  //   order: 'relevance'
+  // })
+  // const [page, setPage] = useState(1);
+  // const [search, setSearch] = useState(false);
 
-  const [imageArr, setImageArr] = useState([]);
+  // const [imageArr, setImageArr] = useState([]);
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "FETCH_START":
+        return {
+          ...state,
+          loading: true
+        };
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          loading: false,
+          data: action.payload.results,
+          isSearch: true
+        };
+        case "FETCH_FAIL":
+        return {
+          loading: false,
+          page: 1,
+          data: null,
+          query: "",
+          order: "relevance",
+          isSearch : false
+        };
+      case "SEARCH":
+        return {
+          ...state,
+          ...action.payload
+        }
+      case "PAGE NEXT":
+        return {
+          ...state,
+          page: state.page + 1
+        }
+
+      case "PAGE PREV":
+        return {
+          ...state,
+          page: state.page === 1 ? 1 : state.page - 1
+        }
+      case "PAGE SET":
+        return {
+          ...state,
+          page: 1
+        }
+      case "IS SEARCHED" :
+        return {
+          ...state, 
+          isSearch : true
+        }
+    }
+  }
+
+  const initialValue = {
+    loading: false,
+    page: 1,
+    data: [],
+    query: "",
+    order: "relevance",
+    isSearch: false
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialValue);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setSval((prev) => ({
-      ...prev,
-      [name]: value
-    }))
+    dispatch( {
+      type : "SEARCH", 
+      payload : {
+        [name] : value
+      }
+    });
   }
 
   const fetchData = async () => {
+    dispatch({type: "FETCH_START"});
     try {
       const res = await Api.get('', {
         params: {
-          query: sVal.query,
-          order: sVal.order,
-          page: page,
+          query: state.query,
+          order: state.order,
+          page: state.page,
           per_page: 20,
           client_id: client_id
         }
       })
       console.log(res.data);
-      setImageArr(res.data.results);
-      setSearch(true);
+      dispatch({
+        type: "FETCH_SUCCESS", 
+        payload : res.data
+      })
+
     }
     catch (err) {
       console.error(err);
       console.log(res);
-      setSearch(false)
+      dispatch({type: "FETCH_FAIL"});
     }
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setPage(1);
-    setSearch(true);
+    dispatch({type: "PAGE SET"});
+    fetchData();
   }
   useEffect(() => {
-    if (search) {
-      fetchData()
-    }
-  }, [page, search])
+      if (state.query) {
+        fetchData()
+      }
+  }, [state.page])
 
 
   return (
@@ -69,7 +138,7 @@ function App() {
           <input type='text' placeholder='search image here'
             name='query'
             className='bg-red-200 p-4 w-72 text-black rounded-2xl text-lg '
-            value={sVal.query}
+            value={state.query}
             onChange={handleChange}
           />
           <div>
@@ -87,25 +156,20 @@ function App() {
 
         {/* Results Section */}
         <ul className='flex gap-6 justify-center w-full flex-wrap m-2'>
-          {imageArr.map((i) => (
+          {state.data && state.data.map((i) => (
             <li key={i.id}>
               <Card src={i.urls.thumb} alt={i.alt_description} title={i.description} down={i.links.download} />
             </li>
           ))}
         </ul>
 
-        {search && <div className='flex gap-10 text-white mb-4'>
+        {state.isSearch && <div className='flex gap-10 text-white mb-4'>
           <button className='bg-red-700 p-2 text-white cursor-pointer'
-            onClick={() => {
-              setPage((prev) => prev === 1 ? 1 : prev - 1);
-
-            }}
+            onClick={() => {dispatch({type : "PAGE PREV"})}}
           >Previous</button>
-          <p>Page No: {page} </p>
+          <p>Page No: {state.page} </p>
           <button className='bg-red-700 p-2 text-white cursor-pointer '
-            onClick={() => {
-              setPage((prev) => prev + 1);
-            }}
+            onClick={() => {dispatch({type : "PAGE NEXT"})}}
           >Next</button>
         </div>}
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
     return (
         <>
             <div className='bg-red-800 border-red-500 border-2  w-64 h-72 p-4 overflow-hidden rounded-lg text-lg item-center'>
-                <a href = {props.down} target='_blank'><img src={props.src} alt={props.alt} className='w-56 h-64' /></a>
+                <a href = {props.down} target='_blank' rel='noreferrer'><img src={props.src} alt={props.alt} className='w-56 h-64' /></a>
                 {/* <p className='text-center text-white font-mono w-3/4'>{props.title}</p> */}
             </div>
         </>
